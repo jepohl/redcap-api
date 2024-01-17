4f99b67ec07dc310bf1dfb918b03d36fcef359bf
jepohl <jack.pohlmann@gmail.com>
2024-01-17 13:08:54 -0500

    updated git ignore to not track working folder, cleaned up export code

--- redcap_export_report.R
+++ redcap_export_report.R
@@ -18,8 +18,9 @@ redcap_export_report <- function(report_id, token = NULL, df_details = TRUE){
   
   response <- httr::POST(url, body = formData, encode = "form")
   redcap <- httr::content(response, encoding = 'UTF-8', 
-                          guess_max = Inf, show_col_types = FALSE #,col_types = paste0(rep('c', 149), collapse = "")
-  )
+                          guess_max = Inf, 
+                          show_col_types = FALSE 
+                          )
   
   if(df_details){
     print(dim(redcap))
