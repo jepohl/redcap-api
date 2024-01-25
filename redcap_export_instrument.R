c8278a360a618b497fd28bf04ae5fb54a5cc9b0d
jepohl <jack.pohlmann@gmail.com>
2024-01-25 16:07:53 -0500

    added function to export full instruments

---
+++ redcap_export_instrument.R
@@ -0,0 +1,46 @@
+
+
+redcap_export_report <- function(url, token = NULL, instrument = NULL, df_details = TRUE){
+  
+  if(is.null(token)){
+    token <- readline(prompt = "Enter unique REDCap API token: ")
+  }
+  
+  if(is.null(instrument)){
+    instrument_names <- get_instruments(url, token)
+    for(i in 1:length(instrument_names)){
+      cat(i, instrument_names[i], "\n")}
+    instrument <- instrument_names[as.numeric(readline(prompt = "Select the desired instrument number: "))]
+  }
+  
+  
+  url <- url #"https://redcap.bumc.bu.edu/api/" or "https://redcap.partners.org/redcap/api/"
+  formData <- list("token"=token,
+                   content='record',
+                   action='export',
+                   format='csv',
+                   type='flat',
+                   csvDelimiter=',',
+                   'fields[0]'='id',
+                   'forms[0]'=instrument,
+                   rawOrLabel='raw',
+                   rawOrLabelHeaders='raw',
+                   exportCheckboxLabel='false',
+                   exportSurveyFields='false',
+                   exportDataAccessGroups='false',
+                   returnFormat='csv'
+  )
+
+  
+  response <- httr::POST(url, body = formData, encode = "form")
+  redcap <- httr::content(response, encoding = 'UTF-8', 
+                          guess_max = Inf, 
+                          show_col_types = FALSE 
+  )
+  
+  if(df_details){
+    print(spec(redcap))
+  }
+  
+  return(redcap)
+}
